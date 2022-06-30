5109ca51b7b6e0a97645602b69bdb0baa9e22992
Steve ChoCho <cyk3233@gmail.com>
2022-06-30 18:23:11 +0900

    Terraform AWS Module EC2

---
+++ output.tf
@@ -0,0 +1,28 @@
+output "account_id" {
+  description = "AWS Account ID"
+  value = var.account_id
+}
+output "vpc_id" {
+  description = "VPC ID"
+  value = var.vpc_id
+}
+
+output "instance_name" {
+  description = "EC2 instance ID"
+  value = var.instance_name
+}
+
+output "instance_type" {
+  description = "EC2 instance type"
+  value = aws_instance.this.instance_type
+}
+
+output "root_device" {
+  description = "EC2 instance type"
+  value = aws_instance.this.root_block_device
+}
+
+output "subnet" {
+  description = "EC2 instance type"
+  value = aws_instance.this.subnet_id
+}
